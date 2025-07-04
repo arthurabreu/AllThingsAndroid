032e175aa224f5ab62dcd6a6e60dd1f1ea98609e
Arthur Abreu <arthurabreupro@gmail.com>
2025-07-04 17:03:06 -0300

    Improved Home layout

--- commonscreens/src/main/java/com/arthurabreu/commonscreens/ui/screens/generic/home/HomeScreen.kt
+++ commonscreens/src/main/java/com/arthurabreu/commonscreens/ui/screens/generic/home/HomeScreen.kt
@@ -1,10 +1,15 @@
 package com.arthurabreu.commonscreens.ui.screens.generic.home
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.arthurabreu.allthingsandroid.commonscreens.R
 
 @Composable
@@ -19,52 +24,32 @@ fun HomeScreen(
     onTextFieldsClick: () -> Unit,
     onApiShowcaseClick: () -> Unit,
 ) {
-    Column {
-        Button(
-            onClick = { onProfileClick() }
-        ) {
-            Text(stringResource(R.string.go_to_profile))
-        }
-        Button(
-            onClick = { onSettingsClick() }
-        ) {
-            Text(stringResource(R.string.go_to_settings))
-        }
-        Button(
-            onClick = { onDownloadClick() }
-        ) {
-            Text(stringResource(R.string.go_to_downloads))
-        }
-        Button(
-            onClick = { onButtonsClick() }
-        ) {
-            Text(stringResource(R.string.go_to_buttons_example))
-        }
-        Button(
-            onClick = { onListsClick() }
-        ) {
-            Text(stringResource(R.string.go_to_lists_example))
-        }
-        Button(
-            onClick = { onLoginsClick() }
-        ) {
-            Text(stringResource(R.string.go_to_logins_example))
-        }
-        Button(
-            onClick = { onLoginsClick() }
-        ) {
-            Text(stringResource(R.string.go_to_logins_fake_example))
-        }
-        Button(
-            onClick = { onTextFieldsClick() }
-        ) {
-            Text(stringResource(R.string.go_to_text_fields_example))
-        }
-        Button(
-            onClick = { onApiShowcaseClick() }
-        ) {
-            Text(stringResource(R.string.go_to_api_showcase_example))
-        }
+    val menuButtons = listOf(
+        MenuButtonInfo(R.string.go_to_profile, onProfileClick),
+        MenuButtonInfo(R.string.go_to_settings, onSettingsClick),
+        MenuButtonInfo(R.string.go_to_downloads, onDownloadClick),
+        MenuButtonInfo(R.string.go_to_buttons_example, onButtonsClick),
+        MenuButtonInfo(R.string.go_to_lists_example, onListsClick),
+        MenuButtonInfo(R.string.go_to_logins_example, onLoginsClick),
+        MenuButtonInfo(R.string.go_to_logins_fake_example, onLoginFakeClick),
+        MenuButtonInfo(R.string.go_to_text_fields_example, onTextFieldsClick),
+        MenuButtonInfo(R.string.go_to_api_showcase_example, onApiShowcaseClick)
+    )
 
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically)
+    ) {
+        item {
+            menuButtons.forEach { buttonInfo ->
+                MenuButton(
+                    text = stringResource(id = buttonInfo.textResId),
+                    onClick = buttonInfo.onClick
+                )
+            }
+        }
     }
 }
